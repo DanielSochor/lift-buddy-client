d8375fb2a21f77dc7c07a7b89fe7e2b388170b97
Daniel Sochor <danielsochor@gmail.com>
2019-09-11 10:02:17 -0500

    remove excess fluff from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Auth from './authorizer';
 
@@ -13,18 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
            <button onClick={authSubmit}>Submit</button>
-        </a>
       </header>
     </div>
   );
--- src/authorizer.js
+++ src/authorizer.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
-const baseUrl = process.env.REACT_APP_SERVER_URL|| '/'
+//const baseUrl = process.env.REACT_APP_SERVER_URL|| '/'
+const baseUrl = 'http://localhost:3000/'
 
-function Auth(){
+function Auth() {
 
-axios.get(baseUrl+ 'api/user').then(response => {
-
-}).catch(error => {
-    console.log(error);
-
-      });
-    }
+    axios.get(baseUrl + 'api/user')
+    .then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+    });
+}
 
 export default Auth;
