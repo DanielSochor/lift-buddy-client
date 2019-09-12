ccc992cb6984d1a27731b8f27cb6a79150992f3d
Daniel Sochor <danielsochor@gmail.com>
2019-09-12 11:47:58 -0500

    return authorizer to standard function format

--- src/authorizer.js
+++ src/authorizer.js
@@ -3,15 +3,14 @@ import axios from 'axios';
 //const baseUrl = process.env.REACT_APP_SERVER_URL|| '/'
 const baseUrl = 'https://healthy-people-back-end.herokuapp.com/'
 
-    (function (obj) {
-        obj.test = () => {
-            axios.get(baseUrl + 'api/user')
-                .then(response => {
-                    console.log(response);
-                }).catch(error => {
-                    console.log(error);
-                });
-        }
-    })(Auth);
+function Auth() {
 
-    export default Auth;
+    axios.get(baseUrl + 'api/user')
+    .then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+export default Auth;
