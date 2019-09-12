c8917ff5698c360c967ef3e161a37057f81fc0f3
Daniel Sochor <danielsochor@gmail.com>
2019-09-12 10:25:57 -0500

    try hardcoding backend url to remove 404 error

--- src/authorizer.js
+++ src/authorizer.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = process.env.REACT_APP_SERVER_URL|| '/'
+//const baseUrl = process.env.REACT_APP_SERVER_URL|| '/'
+const baseUrl = 'https://healthy-people-back-end.herokuapp.com/'
 
 function Auth() {
 
