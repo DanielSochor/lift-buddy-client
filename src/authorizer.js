5cf9f3a04b9c785eb65780a2f6dfdf4d2eec7bfd
Daniel Sochor <danielsochor@gmail.com>
2019-09-17 09:57:38 -0500

    try post route

--- src/authorizer.js
+++ src/authorizer.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-//const baseUrl = process.env.REACT_APP_SERVER_URL|| '/'
-const baseUrl = 'https://healthy-people-back-end.herokuapp.com/'
+const baseUrl = process.env.REACT_APP_SERVER_URL|| '/'
+//const baseUrl = 'https://healthy-people-back-end.herokuapp.com/'
 
 function Auth() {
-    axios.get(baseUrl + 'api/user')
+    console.log('auth called');
+    let test = {
+        'email_address' : 'test'
+      };
+    axios.post(baseUrl + 'api/user',test)
     .then(response => {
         console.log(response);
     }).catch(error => {
