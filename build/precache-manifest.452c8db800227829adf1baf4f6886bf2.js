1dd5c4dec518cb26ccd1ffd087a7670793e47d57
Daniel Sochor <danielsochor@gmail.com>
2019-10-16 17:10:28 -0500

    sending all fo front end changes to test if heroku works

---
+++ build/precache-manifest.452c8db800227829adf1baf4f6886bf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ea53b9c5c96f2cf66e3063930f890b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09cf2e3f1280a7818221",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d4ec8627ed327a4d0106",
+    "url": "/static/js/2.c0736654.chunk.js"
+  },
+  {
+    "revision": "09cf2e3f1280a7818221",
+    "url": "/static/js/main.401c3168.chunk.js"
+  },
+  {
+    "revision": "743fed76d9c76dfe6d12",
+    "url": "/static/js/runtime~main.1d0d2fd8.js"
+  }
+]);
--- src/authorizer.js
+++ src/authorizer.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
-const baseUrl = process.env.REACT_APP_SERVER_URL|| '/'
+const baseUrl = process.env.REACT_APP_SERVER_URL || '/'
 //const baseUrl = 'https://healthy-people-back-end.herokuapp.com/'
 
 function Auth() {
-    console.log('auth called');
+    //console.log('auth called');
     let test = {
-        email_address : 'danielsochor@gmail.com'
-      };
-      console.log('test is: ' + test);
-      console.log('test 0 key is: ' + Object.keys(test)[0]);
-      console.log('test 0 value is: ' + Object.values(test)[0]);
-    axios.post(baseUrl + 'api/user',test)
-    .then(response => {
-        console.log(response);
-    }).catch(error => {
-        console.log(error);
-    });
+        email_address: 'danielsochor@gmail.com'
+    };
+    console.log('test 0 key is: ' + Object.keys(test)[0]);
+    console.log('test 0 value is: ' + Object.values(test)[0]);
+    axios.post(baseUrl + 'api/user', test)
+        .then(response => {
+            console.log('response is: ');
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        });
 }
 
 export default Auth;
