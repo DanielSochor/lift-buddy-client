6ef9b053f00167b5b7873f0fca50e1324b7bc57d
Daniel Sochor <danielsochor@gmail.com>
2019-10-21 09:52:56 -0500

    changes to server file

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-require('dotenv').config()
+require('dotenv').config();
+console.log(process.env.NODE_ENV);
+console.log(process.env.REACT_APP_SERVER_URL );
+const baseUrl = process.env.REACT_APP_SERVER_URL || '/'
+axios.get(baseUrl + 'api/user', test);
