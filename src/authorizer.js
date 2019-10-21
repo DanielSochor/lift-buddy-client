208371cbb0916183b8f00f7bbe669484bfb0ed0e
Daniel Sochor <danielsochor@gmail.com>
2019-10-21 11:21:04 -0500

    will hard coded back end url work without new build?

--- src/authorizer.js
+++ src/authorizer.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 //const baseUrl = process.env.REACT_APP_SERVER_URL
 //const baseUrl = process.env.REACT_APP_SERVER_URL || '/'
-//const baseUrl = 'https://healthy-people-back-end.herokuapp.com/'
+const baseUrl = 'https://healthy-people-back-end.herokuapp.com/'
 
-const baseUrl = (process.env.NODE_ENV === 'production')? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL
+//const baseUrl = (process.env.NODE_ENV === 'production')? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL
 
 function Auth() {
     let test = {
