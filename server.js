db1b2a54c72430c7703de0a550fc05d59375501e
Daniel Sochor <danielsochor@gmail.com>
2019-10-21 13:39:44 -0500

    empty server file

--- server.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-console.log(process.env.NODE_ENV);
-console.log(process.env.REACT_APP_SERVER_URL );
-const baseUrl = process.env.REACT_APP_SERVER_URL || '/'
-axios.get(baseUrl + 'api/user', test);
