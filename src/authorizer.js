0470503cd8092993822c1ffc528e457a83aa4e13
Daniel Sochor <danielsochor@gmail.com>
2019-10-25 05:47:16 -0500

    try removing terinary conditional

--- src/authorizer.js
+++ src/authorizer.js
@@ -8,9 +8,9 @@ console.log(process.env.REACT_APP_LOCAL_URL);
 console.log('process.env.REACT_APP_SERVER_URL is: ');
 console.log(process.env.REACT_APP_SERVER_URL);
 
-var baseUrl = (process.env.NODE_ENV == 'production') ? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL;
+//var baseUrl = (process.env.NODE_ENV == 'production') ? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL;
 
-//var baseUrl = '';
+var baseUrl = '';
 
 if (process.env.NODE_ENV === 'production') {
     baseUrl = process.env.REACT_APP_SERVER_URL;
