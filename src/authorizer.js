9184f3b6d7219e94c36ba5050107c1ecca48f6f4
Daniel Sochor <danielsochor@gmail.com>
2019-10-28 10:24:42 -0500

    add baseURL string definition back

--- src/authorizer.js
+++ src/authorizer.js
@@ -13,7 +13,7 @@ console.log(process.env.REACT_APP_SERVER_URL);
 
 //var baseUrl = (process.env.NODE_ENV == 'production') ? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL;
 
-//var baseUrl = '';
+var baseUrl = '';
 
 // if (process.env.NODE_ENV === 'development') {
 //     baseUrl = process.env.REACT_APP_SERVER_URL;
