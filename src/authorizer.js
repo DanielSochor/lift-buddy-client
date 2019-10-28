ab4e8aae03c3fb83a908b561d97412e748caf314
Daniel Sochor <danielsochor@gmail.com>
2019-10-28 08:14:33 -0500

    What if we don't set baseURL, will dev or prod still work?

--- src/authorizer.js
+++ src/authorizer.js
@@ -15,15 +15,15 @@ console.log(process.env.REACT_APP_SERVER_URL);
 
 var baseUrl = '';
 
-if (process.env.NODE_ENV === 'development') {
-    baseUrl = process.env.REACT_APP_SERVER_URL;
-    console.log('local');
-    console.log(baseUrl);
-} else {
-    baseUrl = process.env.REACT_APP_LOCAL_URL;
-    console.log('production');
-    console.log(baseUrl);
-}
+// if (process.env.NODE_ENV === 'development') {
+//     baseUrl = process.env.REACT_APP_SERVER_URL;
+//     console.log('local');
+//     console.log(baseUrl);
+// } else {
+//     baseUrl = process.env.REACT_APP_LOCAL_URL;
+//     console.log('production');
+//     console.log(baseUrl);
+// }
 
 console.log(process.env.NODE_ENV);
 
