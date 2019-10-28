0015ae73e06ecec8e23ee3d9c545484bc391a975
Daniel Sochor <danielsochor@gmail.com>
2019-10-27 20:45:48 -0500

    conditional adjustment

--- src/authorizer.js
+++ src/authorizer.js
@@ -12,13 +12,13 @@ console.log(process.env.REACT_APP_SERVER_URL);
 
 var baseUrl = '';
 
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'development') {
     baseUrl = process.env.REACT_APP_SERVER_URL;
-    console.log('production');
+    console.log('local');
     console.log(baseUrl);
 } else {
     baseUrl = process.env.REACT_APP_LOCAL_URL;
-    console.log('local');
+    console.log('production');
     console.log(baseUrl);
 }
 
