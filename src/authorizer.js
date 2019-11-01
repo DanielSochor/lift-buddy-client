b67a83b508cd7c3493c488ea0cacd013de125fca
Daniel Sochor <danielsochor@gmail.com>
2019-11-01 10:58:50 -0500

    set baseURL to server without build

--- src/authorizer.js
+++ src/authorizer.js
@@ -12,7 +12,7 @@ console.log(process.env.REACT_APP_SERVER_URL);
 
 //var baseUrl = (process.env.NODE_ENV == 'production') ? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL;
 
-var baseUrl = process.env.REACT_APP_SERVER_URL;
+var baseUrl = process.env.REACT_APP_LOCAL_URL;
 
 // if (process.env.NODE_ENV === 'development') {
 //     baseUrl = process.env.REACT_APP_LOCAL_URL;
