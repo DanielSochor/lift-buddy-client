e3c4c9841a3d575ccb15129a4f4dab735c500d8c
Daniel Sochor <danielsochor@gmail.com>
2019-11-01 10:34:40 -0500

    try build with URL set to local, does it work?

--- build/precache-manifest.7675b540a32f2fd847d2e2171cf92dfc.js
+++ build/precache-manifest.7675b540a32f2fd847d2e2171cf92dfc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6a7fe315597db31fcc27216e3096bc9b",
+    "revision": "6f43b2a314291e7ff704c1826fc14378",
     "url": "https://healthy-people-front-end.herokuapp.com/index.html"
   },
   {
-    "revision": "5c4e4dcf29fcac92e9f3",
+    "revision": "665b663ada375a32f2ae",
     "url": "https://healthy-people-front-end.herokuapp.com/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://healthy-people-front-end.herokuapp.com/static/js/2.c0736654.chunk.js"
   },
   {
-    "revision": "5c4e4dcf29fcac92e9f3",
-    "url": "https://healthy-people-front-end.herokuapp.com/static/js/main.9fd8cc11.chunk.js"
+    "revision": "665b663ada375a32f2ae",
+    "url": "https://healthy-people-front-end.herokuapp.com/static/js/main.97f57898.chunk.js"
   },
   {
     "revision": "7dbbb7169aae17611431",
