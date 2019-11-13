e8a0bf8a0a6e35fac6c829136dd9c080addea9de
Daniel Sochor <danielsochor@gmail.com>
2019-11-13 13:12:05 -0600

    clean up of auth file

--- src/authorizer.js
+++ src/authorizer.js
@@ -1,76 +1,39 @@
-//require('../../LiftBuddy-Client/dotenv');
 import axios from 'axios';
 
 console.log('process.env is: ');
 console.log(process.env);
-console.log('process.env.NODE_ENV is: ');
-console.log(process.env.NODE_ENV);
-console.log('process.env.REACT_APP_LOCAL_URL is: ');
-console.log(process.env.REACT_APP_LOCAL_URL);
-console.log('process.env.REACT_APP_SERVER_URL is: ');
-console.log(process.env.REACT_APP_SERVER_URL);
 
+//Can we somehow include this in the future:
 //var baseUrl = (process.env.NODE_ENV === 'production') ? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL;
 
-// var baseUrl = process.env.REACT_APP_LOCAL_URL;
-
-// var baseUrl = '';
-
-// if (process.env.NODE_ENV === 'development') {
-//      baseUrl = process.env.REACT_APP_LOCAL_URL;
-//      console.log('local');
-//      console.log(baseUrl);
-//     } else {
-//      baseUrl = process.env.REACT_APP_SERVER_URL;     
-//      console.log('production');
-//      console.log(baseUrl);
-//     }
-
-//console.log('baseURL is: ');
-//console.log(baseURL);
-
-//console.log(process.env.NODE_ENV);
-
 function Auth() {
-    //var baseUrl = (process.env.NODE_ENV === 'production') ? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL;
     if (process.env.NODE_ENV === 'production') {
         var baseURL = process.env.REACT_APP_SERVER_URL
-
-        //console.log('baseURL is: ');
-        //console.log(baseURL);
-        console.log('env is prod');
         let test = {
-            email_address: 'Prod@gmail.com'
+            email_address: 'production@gmail.com'
         };
         axios.post(baseURL + 'api/user', test)
             .then(response => {
-                console.log('production URL is: ');
-                console.log(baseURL);
                 console.log('response is: ');
                 console.log(response);
             }).catch(error => {
                 console.log(error);
             });
     } else if (process.env.NODE_ENV === 'development') {
-        console.log('env is dev');
+        var baseURL = process.env.REACT_APP_LOCAL_URL
         let test = {
-            email_address: 'Dev@gmail.com'
+            email_address: 'development@gmail.com'
         };
-        axios.post(process.env.REACT_APP_LOCAL_URL + 'api/user', test)
+        axios.post(baseURL + 'api/user', test)
             .then(response => {
-                console.log('server URL is: ');
-                console.log(process.env.REACT_APP_LOCAL_URL);
                 console.log('response is: ');
                 console.log(response);
             }).catch(error => {
                 console.log(error);
             });
     } else {
-        console.log('env is something else');
+        console.log('development environment is not production or development');
     };
-
-    // console.log('baseURL is: ');
-    // console.log(baseUrl);
 }
 
 export default Auth;
