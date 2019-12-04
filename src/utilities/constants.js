bb351f28287d0d3fbfcca340b124960aae24ab59
Daniel Sochor <danielsochor@gmail.com>
2019-12-03 21:45:32 -0600

    add baseURL to constants

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,9 +1,12 @@
+// set baseURL
+const baseURL = (process.env.NODE_ENV === 'production') ? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL;
+
 // Notification strings for pubsub
 export const NOTIF = {
 };
 
 // API routes
-export const API = {
+export const API = baseURL + {
     login: 'api/user/login',
     logout: 'api/user/logout',
     signup: 'api/user/signup'
