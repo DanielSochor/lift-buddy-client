e434861fdb07014023df3599c15412ca0a182921
Daniel Sochor <danielsochor@gmail.com>
2019-12-03 21:56:45 -0600

    remove baseURL from constants

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -6,7 +6,7 @@ export const NOTIF = {
 };
 
 // API routes
-export const API = baseURL + {
+export const API = {
     login: 'api/user/login',
     logout: 'api/user/logout',
     signup: 'api/user/signup'
