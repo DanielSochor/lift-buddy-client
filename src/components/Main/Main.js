eb7d36e2f8ca5fd26652261017ee2069ebcc0f12
Daniel Sochor <danielsochor@gmail.com>
2019-12-19 16:12:53 -0600

    remove NavBar and add NOTIF import to main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 //-import Run from "../../pages/Run/Run";
 import Pubsub from '../../utilities/pubsub';
 import LogInSignUpModal from '../LogInSignUpModal/LogInSignUpModal';
-import NavBar from '../NavBar/NavBar';
 //import Challenge from '../Challenge/Challenge';
 //import Auth from '../../utilities/authorizer';
 //import HomePage from "../../pages/HomePage/HomePage";
@@ -14,6 +13,7 @@ import NavBar from '../NavBar/NavBar';
 //import ChallengeContainer from '../ChallengeContainer/ChallengeContainer';
 //-import FAB from '../FloatingButton/Fab';
 //import { BrowserRouter as Router, Route } from "react-router-dom";
+import { NOTIF } from '../../utilities/constants';
 
 function Main() {
 
@@ -70,7 +70,6 @@ function Main() {
           <Route exact path="/homepage" component={HomePage}/>
         </div>
       </Router> */}
-      <NavBar />
       <LogInSignUpModal />
       {/* <FAB /> */}
       {/* {pageDirector()} */}
