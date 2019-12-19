d7e0b42fe2086d43159a139d1e9df8e4f1615e09
Daniel Sochor <danielsochor@gmail.com>
2019-12-19 16:14:13 -0600

    remove extraneous comments from App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import './App.css';
-//import Auth from './authorizer';
 
-// function App() {
-
-//   const authSubmit = (event) => {
-//     event.preventDefault();
-//     Auth();
-//   }
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//            <button onClick={authSubmit}>Submit</button>
-//       </header>
-//     </div>
-//   );
-// }
-
-//import Navbar from './components/Navbar/Navbar';
-import Main from './components/Main/Main';
 import NavBar from './components/NavBar/NavBar';
+import Main from './components/Main/Main';
 
 function App() {
   return (
