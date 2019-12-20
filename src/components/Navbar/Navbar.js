b5fb7822f2eeb0e755fecc50de44fc2bef6cb6ee
Daniel Sochor <danielsochor@gmail.com>
2019-12-20 14:39:13 -0600

    progress towards navbar appearing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,23 +9,22 @@ import React from 'react';
 //TODO: if session token doesn't exist show LogInSignUpModal
 //TOFO: if session token does exisit show log out button
 
+const authSubmit = (event) => {
+  event.preventDefault();
+}
+
+
 function NavBar() {
 
   return (
-    <nav className='navbar navbar-expand navbar-dark bg-dark'>
-      {/* <NavbarBrand /> */}
+    <div>
+      <nav>
+        {/* <NavbarBrand /> */}
+        <div className='nav-wrapper'>
 
-      <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-        <span className='navbar-toggler-icon'></span>
-      </button>
-
-      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul className='navbar-nav ml-auto'>
-          {/* <NavbarAuthDropdown /> */}
-        </ul>
-      </div>
-      {/* <AuthModal /> */}
-    </nav>
+        </div>
+      </nav>
+    </div>
   );
 }
 
