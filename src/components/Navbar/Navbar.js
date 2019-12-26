5a5a5a1ea73078b9b4bc4cdc9fa7694a9fc72293
Daniel Sochor <danielsochor@gmail.com>
2019-12-26 14:49:00 -0600

    remove unused items from navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,15 +18,6 @@ function NavBar() {
   return (
     <nav className='navbar navbar-expand navbar-dark bg-dark'>
       <NavbarLogo />
-      <div className='nav-wrapper'>
-
-              <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-        <span className='navbar-toggler-icon'></span>
-      </button>
-
-        {/* <button className="btn waves-effect waves-light light-blue darken-3" type="submit" name="action" id="signUpBtn" href="/homepage" onClick={authSubmit}>Submit<i className="material-icons right">send</i></button> */}
-
-      </div>
     </nav>
   );
 }
