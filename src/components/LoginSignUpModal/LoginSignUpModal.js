7a404dc0294468776fa4500b7577da2691514a2c
Daniel Sochor <danielsochor@gmail.com>
2019-12-26 13:26:49 -0600

    uncomment out and use custom Style to place modal in center of page

--- src/components/LoginSignUpModal/LoginSignUpModal.js
+++ src/components/LoginSignUpModal/LoginSignUpModal.js
@@ -13,13 +13,14 @@ const changeTypeBtnTextValues = {
 
 const customStyles = {
     content: {
-        // top: '50%',
-        // left: '50%',
-        // right: 'auto',
-        // bottom: 'auto',
-        // marginRight: '-50%',
-        // transform: 'translate(-50%, -50%)',
-        // minWidth: '350px'
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        // Set min modal width
+        minWidth: '350px'
     }
 };
 
