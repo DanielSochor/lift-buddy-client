44813a06921997f8bcedb20a622b896bf3b9e52f
Daniel Sochor <danielsochor@gmail.com>
2019-12-27 10:28:08 -0600

    navbar logo has white text

--- src/components/NavBarLogo/NavBarLogo.js
+++ src/components/NavBarLogo/NavBarLogo.js
@@ -14,7 +14,7 @@ function NavbarLogo() {
   }
 
   return (
-    <button className='navbar-logo' onClick={logoClicked}>{APP_NAME}</button>
+    <button className='navbar-logo btn btn-dark' onClick={logoClicked}>{APP_NAME}</button>
   );
 }
 
